3000e667a126e4755e08409cea93a89051014b41
arthurdoelp <arthurdoelp@gmail.com>
2020-11-17 18:17:31 -0500

    Added Sofar Sounds project to portfolio and cleaned up the json a bit. Also updated my email link in the footer component.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,7 +7,7 @@ function Footer(props) {
       <div className="footer">
       <h5>Arthur Doelp's Portfolio</h5>
         <a href="https://github.com/arthurdoelp"><img alt="github" src="../../../Images/github.ico" /></a>
-        <a href="mailto:doelp.a@husky.neu.edu?Subject=Hello"><img alt="email" src="../../../Images/Email.png" /></a>
+        <a href="mailto:arthur@arthurdoelp.com?Subject=Hello"><img alt="email" src="../../../Images/Email.png" /></a>
         <a href="https://www.facebook.com/arthur.doelp"><img alt="facebook" src="../../../Images/Facebook.png" /></a>
         <a href="https://twitter.com/arthurdoelp"><img alt="twitter" src="../../../Images/Twitter.png" /></a>
         <a href="https://www.linkedin.com/in/arthurdoelp/"><img alt="linkedin" src="../../../Images/Linkedin.png" style={{width:"35px"}} /></a>
